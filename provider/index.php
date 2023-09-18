2ad3fd5e85028a7d1e8dcfc64bdc24a47c319223
juggernautsei <sherwingaddis@gmail.com>
2023-09-18 16:49:03 -0400

    provider login page

--- provider/index.php
+++ provider/index.php
@@ -51,24 +51,6 @@ if ($_POST['clinic'] && is_dir('../sites/' . $_POST['clinic'])) {
             </div>
             <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
                 <form method="POST" action="index.php" name="provider_login">
-                    <!--<div class="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
-                        <p class="lead fw-normal mb-0 me-3">Sign in with</p>
-                        <button type="button" class="btn btn-primary btn-floating mx-1">
-                            <i class="fab fa-facebook-f"></i>
-                        </button>
-
-                        <button type="button" class="btn btn-primary btn-floating mx-1">
-                            <i class="fab fa-twitter"></i>
-                        </button>
-
-                        <button type="button" class="btn btn-primary btn-floating mx-1">
-                            <i class="fab fa-linkedin-in"></i>
-                        </button>
-                    </div>
-
-                    <div class="divider d-flex align-items-center my-4">
-                        <p class="text-center fw-bold mx-3 mb-0">Or</p>
-                    </div>-->
 
                     <!-- Email input -->
                     <div class="form-outline mb-4">
@@ -77,29 +59,9 @@ if ($_POST['clinic'] && is_dir('../sites/' . $_POST['clinic'])) {
                         <label class="form-label" for="form3Example3">Clinic Name</label>
                     </div>
 
-                    <!-- Password input -->
-                    <!--<div class="form-outline mb-3">
-                        <input type="password" id="form3Example4" class="form-control form-control-lg"
-                               placeholder="Enter password" />
-                        <label class="form-label" for="form3Example4">Password</label>
-                    </div>-->
-
-                    <!--<div class="d-flex justify-content-between align-items-center">
-
-                        <div class="form-check mb-0">
-                            <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
-                            <label class="form-check-label" for="form2Example3">
-                                Remember me
-                            </label>
-                        </div>
-                        <a href="#!" class="text-body">Forgot password?</a>
-                    </div>-->
-
                     <div class="text-center text-lg-start mt-4 pt-2">
                         <input type="submit" class="btn btn-primary btn-lg"
                                 style="padding-left: 2.5rem; padding-right: 2.5rem;" value="Continue">
-                        <!--<p class="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="#!"
-                                                                                          class="link-danger">Register</a></p>-->
                     </div>
 
                 </form>
@@ -113,23 +75,6 @@ if ($_POST['clinic'] && is_dir('../sites/' . $_POST['clinic'])) {
             Copyright © 2020. All rights reserved.
         </div>
         <!-- Copyright -->
-
-        <!-- Right -->
-        <!--<div>
-            <a href="#!" class="text-white me-4">
-                <i class="fab fa-facebook-f"></i>
-            </a>
-            <a href="#!" class="text-white me-4">
-                <i class="fab fa-twitter"></i>
-            </a>
-            <a href="#!" class="text-white me-4">
-                <i class="fab fa-google"></i>
-            </a>
-            <a href="#!" class="text-white">
-                <i class="fab fa-linkedin-in"></i>
-            </a>
-        </div>-->
-        <!-- Right -->
     </div>
 </section>
 </body>
