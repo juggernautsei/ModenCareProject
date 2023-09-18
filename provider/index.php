d97bcfec37cad31fcc5e2e6fc5c08a63a512d907
juggernautsei <sherwingaddis@gmail.com>
2023-09-18 16:35:14 -0400

    provider login page

--- provider/index.php
+++ provider/index.php
@@ -8,7 +8,7 @@ require_once '../interface/globals.php';
 
 use OpenEMR\Core\Header;
 
-if (!empty($_POST['clinic']) && is_dir($_POST['clinic'])) {
+if (!empty($_POST['clinic'])) {
     echo "Find me";
     die('Clinic found');
 }
