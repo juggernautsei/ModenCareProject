1d1bbbea59fd4be93d275a0b3c07363844561167
juggernautsei <sherwingaddis@gmail.com>
2023-09-29 18:25:04 -0400

    provider login page

--- provider/index.php
+++ provider/index.php
@@ -21,7 +21,7 @@ if ($_POST['clinic'] && is_dir('../sites/' . $_POST['clinic'])) {
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Moden EHR</title>
     <title><?php echo xlt('Provider Login'); ?></title>
     <?php Header::setupHeader(); ?>
     <style>
